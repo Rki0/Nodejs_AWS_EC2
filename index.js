0eb00d7bdeb79e98172dec7feecc4c95c6519ba8
Rki0 <pky00823@gmail.com>
2022-07-16 14:21:27 +0900

    index.js revise

--- index.js
+++ index.js
@@ -12,15 +12,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-const config = require("./config/key");
-
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(config.mongoURI)
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log("Error", err));
-
 //////// mongoDB URI값 가져오기 ////////
 const config = require("./config/key");
 ////////////////
